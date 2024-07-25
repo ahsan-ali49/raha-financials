0939b2d1e6dcba406a14f6b8a15ddb2e650a5657
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-07-25 14:09:40 +0500

    added animation

--- scripts.js
+++ scripts.js
@@ -4,12 +4,11 @@ const loadPage = (page, section) => {
     fetch(`${page}.html`)
         .then(response => response.text())
         .then(data => {
-            // if(page=="pricing"){
-            //     const content1 = document.getElementById('content1');
-            //     content1.innerHTML = "";
-            // }
-            content.innerHTML = data;
 
+            // Remove the slide-in class to reset the animation
+            // content.classList.remove('slide-in');
+
+            content.innerHTML = data;
             // Re-attach the event listener after content is loaded
             attachEventListeners();
 
@@ -19,13 +18,55 @@ const loadPage = (page, section) => {
                     sectionElement.scrollIntoView({ behavior: 'smooth' });
                 }
             }
+
             contenLoader();
+ 
         })
         .catch(error => {
             content.innerHTML = '<p>Sorry, the content could not be loaded.</p>';
             console.error('Error loading page:', error);
         });
+
 };
+// const loadPage = (page, section) => {
+//     // Add the fade-out class
+//     content.classList.add('fade-out');
+
+//     // Wait for the fade-out transition to complete
+//     setTimeout(() => {
+//         fetch(`${page}.html`)
+//             .then(response => response.text())
+//             .then(data => {
+//                 content.innerHTML = data;
+
+//                 // Re-attach the event listener after content is loaded
+//                 attachEventListeners();
+
+//                 if (section) {
+//                     const sectionElement = document.getElementById(section);
+//                     if (sectionElement) {
+//                         sectionElement.scrollIntoView({ behavior: 'smooth' });
+//                     }
+//                 }
+
+                // // Remove the fade-out class and add the fade-in class
+                // content.classList.remove('fade-out');
+                // content.classList.add('fade-in');
+//             })
+//             .catch(error => {
+//                 content.innerHTML = '<p>Sorry, the content could not be loaded.</p>';
+//                 console.error('Error loading page:', error);
+//             });
+//     }, 500); // Match this delay with the CSS transition duration
+// };
+
+// Initial page load
+// document.addEventListener('DOMContentLoaded', () => {
+//     loadPage('services');
+//     attachEventListeners();
+// });
+
+
 
 const attachEventListeners = () => {
     const enterpriseButton = document.querySelector('.hero-indv-btn-enterprise');
