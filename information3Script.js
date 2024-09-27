7579ba8e5ff0a6d3586bc03a42ce51f6d61d94be
Ahsan Ali <ahsanali.bscssef20iba-suk.edu.pk>
2024-09-27 14:16:40 +0500

    removed old files

--- information3Script.js
+++
@@ -1,5 +0,0 @@
-import { formData } from "./informationScript";
-const clientsNameSpan = document.querySelector(".client-name-span");
-if (clientsNameSpan) {
-  clientsNameSpan.textContent = formData.fullName;
-}
